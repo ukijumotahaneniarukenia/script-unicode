5abfa10e56f098bd1279b90ee58e7cacd8fdc51b
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-09-05 18:42:05 +0900

    sketch

--- uni-locale-codepoint-range-info/nnn/src/main/java/app/App.java
+++ uni-locale-codepoint-range-info/nnn/src/main/java/app/App.java
@@ -8,9 +8,18 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static app.AppAvailableLocale.getAvailableLocaleMap;
+
 public class App {
 
-    private static final String SEPARATOR = " ";
+    private static final String PROGRAM_NAME = "uni-locale-codepoint-range-info";
+
+    private static final String PROGRAM_VERSION = "-1-0-0";
+
+    private static final String PROGRAM_CMD = "java -jar";
+    private static final String PROGRAM_SUFFIX = "-SNAPSHOT-jar-with-dependencies.jar";
+
+    private static final String OPTION_HELP = "--help";
 
     private static final String UNICODE_PREFIX_CAPITAL = "\\U";
     private static final String UNICODE_PREFIX_NON_CAPITAL = "\\u";
@@ -21,13 +30,15 @@ public class App {
     private static final String UNICODE_ALPHABETICAL_CAPITAL = "\\u0041-\\u005A";
     private static final String UNICODE_ALPHABETICAL_NON_CAPITAL = "\\u0061-\\u007A";
 
+    private static final String PREFIX_WRAPPER_SALT = "[";
+    private static final String SUFFIX_WRAPPER_SALT = "]";
     private static final String PREFIX_REPEAT_SALT = "[:";
     private static final String SUFFIX_REPEAT_SALT = ":]";
     private static final String JOINING_SALT = "+";
+    private static final String SEPARATOR = " ";
     private static final String SPLITTER = "-";
-    private static final String PREFIX_WRAPPER_SALT = "[";
-    private static final String SUFFIX_WRAPPER_SALT = "]";
     private static final String EMPTY = "";
+
     private static final Integer UNICODE_MAX_LENGTH = 8;
 
     private static final String LOCALE_NAME = "localeName";
@@ -48,6 +59,10 @@ public class App {
         add(UNICODE_START_END);
         add(CODE_POINT_START_END);
     }};
+
+    private static String getAvailableLocale(String locale){
+        return Optional.ofNullable(getAvailableLocaleMap().get(locale)).orElse(SPLITTER+FS+locale);
+    }
 
     private static List<Map<String,String>> getLocaleCodePointRangeInfo (ULocale uLocale){
 
@@ -105,37 +120,122 @@ public class App {
         return summaryList;
     }
 
+    private static List<ULocale> excludeOptionalLocaleList = new LinkedList(){{
+    }};
 
-    public static void main( String[] args ) {
+    private static List<ULocale> excludeMustLocaleList = new LinkedList(){{
 
-        ULocale uLocale = ULocale.getDefault();
+    }};
 
-//        ULocale uLocale = new ULocale("af");
+    private static List<ULocale> includeOptionalLocaleList = new LinkedList(){{
+    }};
 
-        List<Map<String,String>> summaryList = getLocaleCodePointRangeInfo(uLocale);
+    private static List<ULocale> includeMustLocaleList = new LinkedList(){{
+        // 利用可能なロケールの一覧取得
+        addAll(Arrays.stream(ULocale.getAvailableLocales()).collect(Collectors.toList()));
+    }};
 
-        int cnt = summaryList.size();
+    private static List<ULocale> includeLocaleList = new LinkedList(){{
+        addAll(includeOptionalLocaleList);
+        addAll(includeMustLocaleList);
+    }};
 
-        for(int i=0; i < cnt; i++){
-            if (i == 0){
+    private static List<ULocale> wantLocaleList = new LinkedList(){{
 
+    }};
+
+    private static boolean excludeOptionalLocale(ULocale uLocale){
+        return excludeOptionalLocaleList.stream().noneMatch(e-> e.equals(uLocale));
+    }
+
+    private static boolean excludeMustLocale(ULocale uLocale){
+        return excludeMustLocaleList.stream().noneMatch(e-> e.equals(uLocale));
+    }
+
+    private static boolean includeLocale(ULocale uLocale){
+        return wantLocaleList.stream().anyMatch(e-> e.equals(uLocale));
+    }
+
+    private static void Usage(List<String> localeList){
+        System.out.println("Usageだよーん" +
+                RS +
+                RS +
+                "LOCALE_LIST:" +
+                RS +
+                RS +
+                Arrays.asList(ULocale.getAvailableLocales()).stream().map(uLocale -> uLocale.getName()).collect(Collectors.joining(SEPARATOR)) +
+                RS +
+                RS +
+                localeList.stream().filter(e->! e.equals(OPTION_HELP)).map(locale->getAvailableLocale(locale)).collect(Collectors.joining(RS + RS)) +
+                RS +
+                RS +
+                PROGRAM_CMD + SEPARATOR + PROGRAM_NAME + PROGRAM_VERSION + PROGRAM_SUFFIX + SEPARATOR + OPTION_HELP + SEPARATOR + "af" + SEPARATOR + "zh" +
+                RS +
+                RS +
+                PROGRAM_CMD + SEPARATOR + PROGRAM_NAME + PROGRAM_VERSION + PROGRAM_SUFFIX + SEPARATOR + OPTION_HELP + SEPARATOR + "zh" +
+                RS +
+                RS +
+                PROGRAM_CMD + SEPARATOR + PROGRAM_NAME + PROGRAM_VERSION + PROGRAM_SUFFIX + SEPARATOR + "zh" +
+                RS
+        );
+        System.exit(0);
+    }
+
+    public static void main(String... cmdLineArgs) {
+
+        List<String> cmdLineArgList = new LinkedList(){{
+            addAll(Arrays.asList(cmdLineArgs));
+        }};
+
+        if(cmdLineArgList.size() == 0){
+            Usage(Arrays.asList(OPTION_HELP));
+        }
+
+        for(String arg : cmdLineArgList){
+            switch (arg){
+                case OPTION_HELP:
+                    Usage(cmdLineArgList);
+                    break;
+                default:
+                    wantLocaleList.add(new ULocale(arg));
+                    break;
+            }
+        }
+
+        List<ULocale> targetLocaleList = includeLocaleList.stream()
+                .filter(uLocale -> excludeMustLocale(uLocale)) //アンマッチ分は除外
+                .filter(uLocale -> excludeOptionalLocale(uLocale)) //アンマッチ分は除外
+                .filter(uLocale -> includeLocale(uLocale)) //引数に指定したロケールが定義ロケールに含まれている分を出力
+                .collect(Collectors.toList());
+
+        int localeCnt = targetLocaleList.size();
+
+        for(int localeNum = 0; localeNum < localeCnt; localeNum++){
+
+            if(localeNum == 0){
                 System.out.println(OUTPUT_HEADER_LIST.stream().collect(Collectors.joining(FS)));
+            }
 
+            List<Map<String,String>> summaryList = getLocaleCodePointRangeInfo(targetLocaleList.get(localeNum));
+
+            int cnt = summaryList.size();
+
+            for(int i=0; i < cnt; i++){
+                {
+                    System.out.print(summaryList.get(i).get(LOCALE_NAME));
+                    System.out.print(FS);
+                    System.out.print(summaryList.get(i).get(DISPLAY_NAME));
+                    System.out.print(FS);
+                    System.out.print(summaryList.get(i).get(DISPLAY_NAME_WITH_DIALECT));
+                    System.out.print(FS);
+                    System.out.print(summaryList.get(i).get(PATTERN_CLASS));
+                    System.out.print(FS);
+                    System.out.print(summaryList.get(i).get(UNICODE_START_END));
+                    System.out.print(FS);
+                    System.out.print(summaryList.get(i).get(CODE_POINT_START_END));
+                }
+                System.out.println();
             }
-            {
-                System.out.print(summaryList.get(i).get(LOCALE_NAME));
-                System.out.print(FS);
-                System.out.print(summaryList.get(i).get(DISPLAY_NAME));
-                System.out.print(FS);
-                System.out.print(summaryList.get(i).get(DISPLAY_NAME_WITH_DIALECT));
-                System.out.print(FS);
-                System.out.print(summaryList.get(i).get(PATTERN_CLASS));
-                System.out.print(FS);
-                System.out.print(summaryList.get(i).get(UNICODE_START_END));
-                System.out.print(FS);
-                System.out.print(summaryList.get(i).get(CODE_POINT_START_END));
-            }
-            System.out.println();
         }
     }
 }
